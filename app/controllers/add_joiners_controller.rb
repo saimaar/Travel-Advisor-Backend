b68ef9f5904e76ff4a53f2fc854743b17aa1855f
Valentine <valentinemaillard1@gmail.com>
2020-03-30 14:39:48 -0400

    added destroy method for addJoiner

--- app/controllers/add_joiners_controller.rb
+++ app/controllers/add_joiners_controller.rb
@@ -1,15 +1,15 @@
 class AddJoinersController < ApplicationController
 
     def index
-        @add_joiners = AddJoiner.all
+        add_joiners = AddJoiner.all
 
-        render json: @add_joiners
+        render json: add_joiners
     end
 
     def show
-        @add_joiner = AddJoiner.find(params[:id])
+        add_joiner = AddJoiner.find(params[:id])
 
-        render json: @add_joiner
+        render json: add_joiner
     end
 
     # create_add_joiner look for the logged in user grab its bucketlist and the id of that bucketlist.
@@ -17,9 +17,17 @@ class AddJoinersController < ApplicationController
     # merge two objects together
     def create
         create_add_joiner = add_joiner_params.merge({bucketlist_id: logged_user.bucketlist.id})
-        @add_joiner = AddJoiner.create(create_add_joiner)
+        add_joiner = AddJoiner.create(create_add_joiner)
 
-        render json: @add_joiner
+        render json: add_joiner
+    end
+
+    def destroy
+        add_joiner = AddJoiner.find(params[:id])
+
+        add_joiner.destroy
+
+        render json: add_joiner
     end
 
     private
