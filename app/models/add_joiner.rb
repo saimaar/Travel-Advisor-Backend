12981a6f93cb70858fca136d6418faac17879994
Valentine <valentinemaillard1@gmail.com>
2020-04-03 15:08:56 -0400

    fixed add_joiner validation

--- app/models/add_joiner.rb
+++ app/models/add_joiner.rb
@@ -1,5 +1,6 @@
 class AddJoiner < ApplicationRecord
   belongs_to :bucketlist
   belongs_to :destination
-  validates :destination_id, uniqueness: true
+  # validates :destination_id, uniqueness: true
+  
 end
