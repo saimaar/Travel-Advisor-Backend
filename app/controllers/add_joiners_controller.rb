e6919984b917be98f397cc0731a0678c0ab5c959
Valentine <valentinemaillard1@gmail.com>
2020-04-03 16:29:43 -0400

    fixed bucketlist duplicate issue

--- app/controllers/add_joiners_controller.rb
+++ app/controllers/add_joiners_controller.rb
@@ -13,7 +13,9 @@ class AddJoinersController < ApplicationController
     end
 
     def create
-        destination = AddJoiner.find_by(destination_id: params[:destination_id])
+        logged_user_add_joiner = logged_user.bucketlist.add_joiners
+        destination = logged_user_add_joiner.find_by(destination_id: params[:destination_id])
+        byebug
         if destination.nil? 
             create_add_joiner = add_joiner_params.merge({bucketlist_id: logged_user.bucketlist.id})
             add_joiner = AddJoiner.create(create_add_joiner)
