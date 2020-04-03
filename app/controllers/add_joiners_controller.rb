22010e844c76ab05d545f9c90f4376f454b5df0e
Valentine <valentinemaillard1@gmail.com>
2020-04-03 16:47:57 -0400

    removed byebug

--- app/controllers/add_joiners_controller.rb
+++ app/controllers/add_joiners_controller.rb
@@ -15,7 +15,7 @@ class AddJoinersController < ApplicationController
     def create
         logged_user_add_joiner = logged_user.bucketlist.add_joiners
         destination = logged_user_add_joiner.find_by(destination_id: params[:destination_id])
-        byebug
+        
         if destination.nil? 
             create_add_joiner = add_joiner_params.merge({bucketlist_id: logged_user.bucketlist.id})
             add_joiner = AddJoiner.create(create_add_joiner)
--- app/models/add_joiner.rb
+++ app/models/add_joiner.rb
@@ -1,5 +1,4 @@
 class AddJoiner < ApplicationRecord
   belongs_to :bucketlist
   belongs_to :destination
-  # validates :destination_id, uniqueness: true
 end
